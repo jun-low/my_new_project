0ee64f57e5ae3af3f0055cc8d9eec97cc143363b
jun low <zhijun.low@gmail.com>
2019-10-07 14:29:45 +0800

    my new page

---
+++ source/javascripts/site.js
@@ -0,0 +1,51 @@
+// This is where it all goes :)
+// Init all plugin
+$(document).on('ready', function () {
+
+    // Show/hide menu when icon is clicked
+    const menuItems   = $('.all-menu-wrapper .nav-link');
+    const menuIcon    = $('.menu-icon, #navMenuIcon');
+    const menuBlock   = $('.all-menu-wrapper');
+    const reactToMenu = $('.page-main, .navbar-sidebar, .page-cover')
+    const menuLinks   = $(".navbar-mainmenu a, .navbar-sidebar a");
+    // Menu icon clicked
+    menuIcon.on('click', function () {
+      menuIcon.toggleClass('menu-visible');
+      menuBlock.toggleClass('menu-visible');
+      menuItems.toggleClass('menu-visible');
+      reactToMenu.toggleClass('menu-visible');
+      return false;
+    });
+  
+    // Hide menu after a menu item clicked
+    menuLinks.on('click', function () {
+      menuIcon.removeClass('menu-visible');
+      menuBlock.removeClass('menu-visible');
+      menuItems.removeClass('menu-visible');
+      reactToMenu.removeClass('menu-visible');
+      return true;
+    });
+  },)
+  
+    //Page Loader : hide loader when all are loaded
+    $(window).on('load', function () {
+      $('.page-loader').addClass('p-hidden');
+      $('.section').addClass('anim');
+    });
+  
+    // cursor position
+    (function() {
+      document.addEventListener("mousemove", parallax);
+      // const elem = document.querySelector("#parallax");
+      function parallax(e) {
+        let w = window.innerWidth/2;
+        let h = window.innerHeight/2;
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+        let x = `${90 - (mouseX - w) * 0.01}% ${100 - (mouseY - h) * 0.01}%`;
+        // console.log(x);
+        document.querySelector("#parallax").style.backgroundPosition = x;
+      }
+  
+    })();
+  
